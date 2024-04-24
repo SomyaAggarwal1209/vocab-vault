74fc6a847d2f56416ccd3a87fd1a0cd606f2734d
SomyaAggarwal1209 <117525704+SomyaAggarwal1209@users.noreply.github.com>
2024-04-24 12:54:26 +0530

    Update main.py
    UI setup

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ canvas.create_text(387, 150, text="Title", font=("Arial", 30, "italic"))  # Corr
 canvas.create_text(387, 256, text="word", font=("Arial", 50, "bold"))    # Corrected the font name
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+
+
+# Buttons
+cross_image = PhotoImage(file="images/cross_button.png")
+smaller_cross_image = cross_image.subsample(3, 3)
+unknown_button = Button(window, image=smaller_cross_image, highlightthickness=0, borderwidth=0, relief="flat")
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/check_button.png")
+smaller_right_button = check_image.subsample(3, 3)
+known_button = Button(window, image=smaller_right_button, highlightthickness=0, borderwidth=0, relief="flat")
+known_button.grid(row=1, column=1)
+
+window.mainloop()
+
+
