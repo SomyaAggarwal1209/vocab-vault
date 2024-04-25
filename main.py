c860723d90239be05cbd9d66fabcf31b0df44b58
SomyaAggarwal1209 <117525704+SomyaAggarwal1209@users.noreply.github.com>
2024-04-25 15:42:15 +0530

    Update main.py
    is_known function

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B0C5A4"
-
-data = pd.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -17,10 +24,18 @@ def next_card():
     canvas.itemconfig(card_bg, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_bg, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 
@@ -45,13 +60,13 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 cross_image = PhotoImage(file="images/cross_button.png")
 smaller_cross_image = cross_image.subsample(3, 3)
 unknown_button = Button(window, image=smaller_cross_image, highlightthickness=0, borderwidth=0, relief="flat",
-                        command=next_card)
+                        command=flip_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/check_button.png")
 smaller_right_button = check_image.subsample(3, 3)
 known_button = Button(window, image=smaller_right_button, highlightthickness=0, borderwidth=0, relief="flat",
-                      command=next_card)
+                      command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
